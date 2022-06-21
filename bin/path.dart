07538ea007ecd3fd1e5028a38612da0354ea70e2
Akramjon <usmonovakramjon2002@gmail.com>
2022-06-21 20:25:19 +0500

    davom ettirishni so'rash qoldi

--- bin/path.dart
+++ bin/path.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:filesize/filesize.dart';
 
 class Path {
+  List<FileSystemEntity> direct = [];
   start() {
     print("Katalogni kiriting:");
     String filePath = stdin.readLineSync()!;
@@ -11,45 +12,77 @@ class Path {
 
   void printFolders(String path) {
     final dir = Directory(path);
-    final List<FileSystemEntity> entities = dir.listSync().toList();
+    final List<FileSystemEntity> directory = dir.listSync().toList();
     print(path.replaceAll("\\", "/").split("/").last);
     List<FileSystemEntity> file = [];
+    int n = 0;
 
-    for (var i = 0; i < entities.length; i++) {
-      var fileSize = filesize(entities[i].statSync().size);
-
-      if (entities[i] is File) {
+    for (var i = 0; i < directory.length; i++) {
+      if (!(directory[i] is File)) {
+        direct.add(directory[i]);
         print(
-            "${i++}📄  |   ${entities[i].toString().exchange().split("/").last} - ${entities[i].statSync().changed} - $fileSize");
-      } else {
-        print(
-            "${i++}📂  |   ${entities[i].toString().exchange().split("/").last} - ${entities[i].statSync().changed}");
+            "${n}📂  |   ${directory[i].toString().exchange().split("/").last} - ${directory[i].statSync().changed}");
+        n++;
       }
     }
-    print("Tartib raqamini kiriting(qaytish uchun '0'): ");
-    String tanlovS = stdin.readLineSync()!;
 
-    if (tanlovS.toLowerCase() == "exit") {
-      exit(0);
-    } else {
-      int tanlov = int.parse(tanlovS);
-      if (tanlov == 0) {
-        printFolders(path.cutLastFolder());
-      } else {
-        if (entities[tanlov - 1] is Directory) {
-          printFolders(entities[tanlov - 1].toString().exchange());
-        } else if (entities[tanlov - 1] is File) {
-          print(entities[tanlov - 1]);
+    var a = 0;
+    for (var i = 0; i < directory.length; i++) {
+      if (directory[i] is File) {
+        direct.add(directory[i]);
+        var fileSize = filesize(directory[i].statSync().size);
+        if (directory[i] is File) {
           print(
-              "Siz tanlagan raqamda  katalog mavjud emas \ndavom ettirish uchun 1 ni bosing?");
-          if (int.parse(stdin.readLineSync()!) == 1) {
-            printFolders(path);
-          } else {
-            exit(0);
-          }
+              "${a++}📄  |   ${directory[i].toString().exchange().split("/").last} - ${directory[i].statSync().changed} - $fileSize");
         }
       }
     }
+
+    for (var i = direct.length - 1; i >= 0; i--) {
+      var first = 0, temp;
+      for (var j = 1; j <= i; j++) {
+        if (direct[j].statSync().size > direct[first].statSync().size) {
+          first = j;
+        }
+
+        temp = direct[first];
+        direct[first] = direct[i];
+        direct[i] = temp;
+      }
+    }
+    print("\n");
+    a = 0;
+    for (var i = 0; i < direct.length - 1; i++) {
+      if (direct[i] is File) {
+        var fileSize = filesize(direct[i].statSync().size);
+        print(
+            "${a++}📄  |   ${direct[i].toString().exchange().split("/").last} - ${direct[i].statSync().changed} - $fileSize");
+      }
+    }
+    // print("Tartib raqamini kiriting(qaytish uchun '0'): ");
+    // String tanlovS = stdin.readLineSync()!;
+
+    // if (tanlovS.toLowerCase() == "exit") {
+    //   exit(0);
+    // } else {
+    //   int tanlov = int.parse(tanlovS);
+    //   if (tanlov == 0) {
+    //     printFolders(path.cutLastFolder());
+    //   } else {
+    //     if (directory[tanlov - 1] is Directory) {
+    //       printFolders(directory[tanlov - 1].toString().exchange());
+    //     } else if (directory[tanlov - 1] is File) {
+    //       print(directory[tanlov - 1]);
+    //       print(
+    //           "Siz tanlagan raqamda  katalog mavjud emas \ndavom ettirish uchun 1 ni bosing?");
+    //       if (int.parse(stdin.readLineSync()!) == 1) {
+    //         printFolders(path);
+    //       } else {
+    //         exit(0);
+    //       }
+    //     }
+    //   }
+    // }
   }
 }
 
